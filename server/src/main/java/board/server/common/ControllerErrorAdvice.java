0b9018d7fed0f16100de3c5a6dc08008c9594083
jiwon31 <swm10099@naver.com>
2023-04-27 15:37:45 +0900

    feat: RestControllerAdvice로 예외 처리

---
+++ server/src/main/java/board/server/common/ControllerErrorAdvice.java
@@ -0,0 +1,24 @@
+package board.server.common;
+
+import board.server.common.exception.BoardNotFoundException;
+import board.server.common.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerErrorAdvice {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorResponse handleUserNotFoundException() {
+        return new ErrorResponse("해당 사용자를 찾을 수 없습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(BoardNotFoundException.class)
+    public ErrorResponse handleBoardNotFoundException() {
+        return new ErrorResponse("해당 게시글을 찾을 수 없습니다.");
+    }
+}
---
+++ server/src/main/java/board/server/common/ErrorResponse.java
@@ -0,0 +1,11 @@
+package board.server.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private final String message;
+}
