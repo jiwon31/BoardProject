18b92f7ce49fe7c46d3744d664fa4dd45037e202
jiwon31 <swm10099@naver.com>
2023-05-18 16:03:24 +0900

    refactor: 게시글 리스트 enabled 조건 변경

--- client/src/hooks/useBoard.tsx
+++ client/src/hooks/useBoard.tsx
@@ -2,15 +2,17 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import BoardApi from "api/board-api";
 import { Board, BoardContent, UpdateBoardRequest } from "types/board";
 import useRecoilUser from "./useRecoilUser";
+import { useLocation } from "react-router-dom";
 
 export default function useBoard(boardId?: number, boardApi = new BoardApi()) {
   const { user } = useRecoilUser();
   const queryClient = useQueryClient();
+  const { pathname } = useLocation();
 
   const boardQuery = useQuery<Board[], Error>(
     ["boards"],
     boardApi.getBoardList,
-    { staleTime: 1000 * 60, enabled: !boardId }
+    { staleTime: 1000 * 60, enabled: pathname === "/" }
   );
 
   const singleBoardQuery = useQuery<Board, Error>(
