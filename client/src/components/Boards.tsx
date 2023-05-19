15809c7c40792b944f3bdd485c5ed054893d62e9
jiwon31 <swm10099@naver.com>
2023-05-19 17:18:05 +0900

    feat: 검색시 삭제된 게시글은 보여주지 않음

--- client/src/components/Boards.tsx
+++ client/src/components/Boards.tsx
@@ -6,12 +6,18 @@ import useSearch from "hooks/useSearch";
 export default function Boards() {
   const { searchParams } = useSearch();
   const {
-    boardQuery: { isLoading, error, data: boards, refetch },
+    boardQuery: { isLoading, error, data: boardList, refetch },
   } = useBoard();
 
   useEffect(() => {
     refetch();
   }, [searchParams, refetch]);
+
+  /** 검색했을 때 삭제된 게시글은 보여주지 않는다. */
+  const boards =
+    searchParams.toString() && boardList
+      ? boardList.filter((board) => !board.isDeleted)
+      : boardList;
 
   return (
     <section className="flex flex-col py-10 max-w-5xl mx-auto">
