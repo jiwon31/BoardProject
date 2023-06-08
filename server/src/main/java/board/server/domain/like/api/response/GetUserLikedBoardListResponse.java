f4edc453fd068ad602ccd81a1b23723f19e76d60
jiwon31 <swm10099@naver.com>
2023-06-08 16:57:12 +0900

    feat: 내가 작성한 글/좋아요한 글 조회 응답에 댓글수,좋아요수 추가

--- server/src/main/java/board/server/domain/like/api/response/GetUserLikedBoardListResponse.java
+++ server/src/main/java/board/server/domain/like/api/response/GetUserLikedBoardListResponse.java
@@ -19,13 +19,21 @@ public class GetUserLikedBoardListResponse {
 
     private final LocalDateTime createdAt;
 
+    private final int likeCount;
+
+    private final int commentCount;
+
     @Builder
-    public GetUserLikedBoardListResponse(Long id, String title, String content, String userName, Boolean isDeleted, LocalDateTime createdAt) {
+    public GetUserLikedBoardListResponse(Long id, String title, String content,
+                                         String userName, Boolean isDeleted, LocalDateTime createdAt,
+                                         int likeCount, int commentCount) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.userName = userName;
         this.isDeleted = isDeleted;
         this.createdAt = createdAt;
+        this.likeCount = likeCount;
+        this.commentCount = commentCount;
     }
 }
--- server/src/main/java/board/server/domain/user/api/response/GetMyBoardListResponse.java
+++ server/src/main/java/board/server/domain/user/api/response/GetMyBoardListResponse.java
@@ -19,13 +19,21 @@ public class GetMyBoardListResponse {
 
     private final LocalDateTime createdAt;
 
+    private final int likeCount;
+
+    private final int commentCount;
+
     @Builder
-    public GetMyBoardListResponse(Long id, String title, String content, String userName, Boolean isDeleted, LocalDateTime createdAt) {
+    public GetMyBoardListResponse(Long id, String title, String content,
+                                  String userName, Boolean isDeleted, LocalDateTime createdAt,
+                                  int likeCount, int commentCount) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.userName = userName;
         this.isDeleted = isDeleted;
         this.createdAt = createdAt;
+        this.likeCount = likeCount;
+        this.commentCount = commentCount;
     }
 }
